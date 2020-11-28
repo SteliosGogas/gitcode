bfbc0e281a8ffb2cfc05e1fbfd6e506fb024783b
unknown <steliosg22@gmil.comgit config --global user.email steliosg22@gmil.com>
2020-11-28 16:01:00 +0200

    Resolved merge conflict by incorporating both suggestions.

--- hungry.py
+++ hungry.py
@@ -11,8 +11,10 @@ else if :
 		print("drink soda")
 		print('drink juice')
 else :
-	tired = input ('do you want to take a nap?')
+	tired = input ('are you tired?')
 	if tired == 'yes' :
-		print('get a nap ')
+		print('get a nap !!!')
+		print('get a nap for 20 min')
+		print('relax in your bed for a little')
 
 
